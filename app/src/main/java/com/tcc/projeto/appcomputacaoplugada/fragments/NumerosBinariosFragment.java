3ee594f5833fed1877b972ffd2fb732c8332e34a
barbosamaatheus <matheus.barbosa@dce.ufpb.br>
2018-09-22 17:16:18 -0300

    Logica tela numeros binarios implementada

--- app/src/main/java/com/tcc/projeto/appcomputacaoplugada/fragments/NumerosBinariosFragment.java
+++ app/src/main/java/com/tcc/projeto/appcomputacaoplugada/fragments/NumerosBinariosFragment.java
@@ -1,31 +1,328 @@
 package com.tcc.projeto.appcomputacaoplugada.fragments;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.tcc.projeto.appcomputacaoplugada.R;
+import com.tcc.projeto.appcomputacaoplugada.activitys.ExerciciosActivity;
+import com.tcc.projeto.appcomputacaoplugada.aplication.MyApplication;
+import com.tcc.projeto.appcomputacaoplugada.objetos.Carta;
 
 
 public class NumerosBinariosFragment extends Fragment {
+    private ImageButton mCarta1, mCarta2, mCarta4, mCarta8, mCarta16;
+    private TextView num1, num2, num4, num8, num16;
+    private RadioGroup radioGroup2, radioGroup3;
+    private RadioButton certo1, certo2, certo3, errado11, errado12, errado21, errado22, errado31, errado32;
+    private Carta carta01, carta02, carta04, carta08, carta16;
+    private TextView perg1, perg2, perg3;
+    private Button finalizar;
+    private EditText mMaiorValor, mMenorValor;
+    private boolean passou2 = true;
+    private boolean passou3 = true;
+    private boolean checked1 = false;
+    private boolean checked3 = false;
+    private boolean checked2 = false;
+    private boolean exibir;
+    private String maiorValor = "";
+    private String menorValor = "";
+    private MyApplication myApplication;
 
     public NumerosBinariosFragment() {
         // Required empty public constructor
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_numeros_binarios, container, false);
+        View view = inflater.inflate(R.layout.fragment_numeros_binarios, container, false);
+
+        initViews(view);
+        if (!respostasIsEmpty()) {
+            validarCampos();
+        }
 
 
+        mCarta1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                virarCarta(carta01, num1, mCarta1);
+            }
+        });
+        mCarta2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                virarCarta(carta02, num2, mCarta2);
 
+            }
+        });
+        mCarta4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                virarCarta(carta04, num4, mCarta4);
+            }
+        });
+        mCarta8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                virarCarta(carta08, num8, mCarta8);
+            }
+        });
+        mCarta16.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                virarCarta(carta16, num16, mCarta16);
+            }
+        });
+        radioGroup2.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                onRadioButtonClicked2(checkedId);
+            }
+        });
+        radioGroup3.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                onRadioButtonClicked3(checkedId);
+            }
+        });
+        finalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (respostasIsEmpty()) {
+                    onCreateDialog();
+                } else {
+                    validarCampos();
+                    gerenciarResultados();
+                }
+            }
+        });
 
         return view;
     }
 
+    private void gerenciarResultados() {
+        if (!exibir) {
+            myApplication.setPositionExercicio(2);
+            callNextFragment();
+        } else {
+            restartFragment();
+        }
+    }
+
+    private boolean respostasIsEmpty() {
+        validarClicked1();
+        boolean isEmpty;
+        if (!checked1 || !checked2 || !checked3) {
+            isEmpty = true;
+        } else {
+            isEmpty = false;
+        }
+        return isEmpty;
+    }
+
+    private boolean validarCampos() {
+        View focus = null;
+        exibir = false;
+        if (!maiorValor.equals("31")) {
+            mMaiorValor.setError("Resposta incorreta");
+            focus = mMaiorValor;
+            exibir = true;
+        }
+        if (!menorValor.equals("0")) {
+            mMenorValor.setError("Resposta incorreta");
+            focus = mMenorValor;
+            exibir = true;
+        }
+        if (!passou2) {
+            perg2.setError("Resposta incorreta");
+            focus = perg2;
+            exibir = true;
+        }
+        if (!passou3) {
+            perg3.setError("Resposta incorreta");
+            focus = perg3;
+            exibir = true;
+        }
+        if (exibir) {
+            focus.requestFocus();
+        }
+        return exibir;
+    }
+
+    private void virarCarta(Carta carta, TextView num, ImageButton mCarta) {
+        alterarEstadoCarta(carta);
+        mCarta.setBackgroundResource(carta.getImagem());
+        if (carta.isFrente()) {
+            num.setText("1");
+        } else {
+            num.setText("0");
+        }
+    }
+
+    private void alterarEstadoCarta(Carta carta) {
+        if (carta.isFrente()) {
+            carta.setFrente(false);
+            carta.setImagem(R.mipmap.fundocarta);
+        } else {
+            carta.setFrente(true);
+            mudarImagem(carta);
+        }
+    }
+
+    private void mudarImagem(Carta carta) {
+        switch (carta.getNumero()) {
+            case 1:
+                carta.setImagem(R.mipmap.carta1);
+                break;
+            case 2:
+                carta.setImagem(R.mipmap.carta2);
+                break;
+            case 4:
+                carta.setImagem(R.mipmap.carta4);
+                break;
+            case 8:
+                carta.setImagem(R.mipmap.carta8);
+                break;
+            case 16:
+                carta.setImagem(R.mipmap.carta16);
+                break;
+        }
+    }
+
+    private void initViews(View view) {
+        myApplication = (MyApplication) getActivity().getApplicationContext();
+        finalizar = (Button) view.findViewById(R.id.btn_finalizar_nb);
+        mMaiorValor = (EditText) view.findViewById(R.id.valorMaiorNB);
+        mMenorValor = (EditText) view.findViewById(R.id.valorMenorNB);
+        initCartas(view);
+        initNum(view);
+        createCartas();
+        initRadioGroups(view);
+        initPerguntas(view);
+
+    }
+
+    private void initPerguntas(View view) {
+        perg1 = (TextView) view.findViewById(R.id.perg1_nb);
+        perg2 = (TextView) view.findViewById(R.id.perg2_nb);
+        perg3 = (TextView) view.findViewById(R.id.perg3_nb);
+    }
+
+    private void initRadioGroups(View view) {
+        radioGroup2 = (RadioGroup) view.findViewById(R.id.myRadioGroup2NB);
+        certo2 = (RadioButton) view.findViewById(R.id.certo2);
+        errado21 = (RadioButton) view.findViewById(R.id.errado21);
+        errado22 = (RadioButton) view.findViewById(R.id.errado22);
+
+        radioGroup3 = (RadioGroup) view.findViewById(R.id.myRadioGroup3NB);
+        certo3 = (RadioButton) view.findViewById(R.id.certo3);
+        errado31 = (RadioButton) view.findViewById(R.id.errado31);
+        errado32 = (RadioButton) view.findViewById(R.id.errado32);
+    }
+
+    private void initNum(View view) {
+        num1 = (TextView) view.findViewById(R.id.num1NB);
+        num2 = (TextView) view.findViewById(R.id.num2NB);
+        num4 = (TextView) view.findViewById(R.id.num4NB);
+        num8 = (TextView) view.findViewById(R.id.num8NB);
+        num16 = (TextView) view.findViewById(R.id.num16NB);
+    }
+
+    private void initCartas(View view) {
+        mCarta1 = (ImageButton) view.findViewById(R.id.carta1NB);
+        mCarta2 = (ImageButton) view.findViewById(R.id.carta2NB);
+        mCarta4 = (ImageButton) view.findViewById(R.id.carta4NB);
+        mCarta8 = (ImageButton) view.findViewById(R.id.carta8NB);
+        mCarta16 = (ImageButton) view.findViewById(R.id.carta16NB);
+    }
+
+    private void createCartas() {
+        carta01 = new Carta(R.id.carta1, R.mipmap.carta1, 1, true);
+        carta02 = new Carta(R.id.carta2, R.mipmap.carta2, 2, true);
+        carta04 = new Carta(R.id.carta4, R.mipmap.carta4, 4, true);
+        carta08 = new Carta(R.id.carta8, R.mipmap.carta8, 8, true);
+        carta16 = new Carta(R.id.carta16, R.mipmap.carta16, 16, true);
+    }
+
+    public void validarClicked1() {
+        if (mMenorValor.getText().toString().isEmpty() || mMaiorValor.getText().toString().isEmpty()) {
+            checked1 = false;
+        } else {
+            this.maiorValor = mMaiorValor.getText().toString();
+            this.menorValor = mMenorValor.getText().toString();
+            checked1 = true;
+        }
+        if (maiorValor.isEmpty() || menorValor.isEmpty()){
+            checked1 = false;
+        }else {
+            checked1 = true;
+        }
+    }
+
+    public void onRadioButtonClicked2(int checked) {
+        checked2 = true;
+        passou2 = false;
+        switch (checked) {
+            case R.id.certo2:
+                passou2 = true;
+                break;
+            case R.id.errado21:
+                passou2 = false;
+                break;
+
+            case R.id.errado22:
+                passou2 = false;
+                break;
+        }
+    }
+
+    public void onRadioButtonClicked3(int checked) {
+        checked3 = true;
+        passou3 = false;
+        switch (checked) {
+            case R.id.certo3:
+                passou3 = true;
+                break;
+            case R.id.errado31:
+                passou3 = false;
+                break;
+            case R.id.errado32:
+                passou3 = false;
+                break;
+        }
+    }
+
+    private void restartFragment() {
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.detach(this).attach(this).commit();
+    }
+
+    public void onCreateDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.DialogStyle);
+        builder.setMessage(R.string.texto_alert_sem_resposta).setTitle("Algo deu errado :(");
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void callNextFragment() {
+        Intent intent = new Intent(getActivity().getApplicationContext(), ExerciciosActivity.class);
+        startActivity(intent);
+    }
 }
