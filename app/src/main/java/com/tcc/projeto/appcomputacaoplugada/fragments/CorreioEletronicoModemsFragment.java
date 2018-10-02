09baeaf7da33b01d71e4d9fea3a13ece408e0eae
Matheus Barbosa <matheus.barbosa@phoebus.local>
2018-10-02 17:32:10 -0300

    Correção de bugs na tela C.E.M

--- app/src/main/java/com/tcc/projeto/appcomputacaoplugada/fragments/CorreioEletronicoModemsFragment.java
+++ app/src/main/java/com/tcc/projeto/appcomputacaoplugada/fragments/CorreioEletronicoModemsFragment.java
@@ -6,6 +6,7 @@ import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,20 +15,24 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.tcc.projeto.appcomputacaoplugada.R;
 import com.tcc.projeto.appcomputacaoplugada.adapter.TabelaAdapter;
 
+import java.text.Normalizer;
+
 public class CorreioEletronicoModemsFragment extends MyFragments {
     private TextInputLayout textInputLayout1, textInputLayout2;
-    private TextInputEditText mTextoTraduzido;
+    private TextInputEditText mTextoTraduzido, mTextoNome;
     private TextView envinado, nomeBinario;
     private Button mEnviar;
     private String textoNome = "";
     private String textoTraduzido = "";
-    private String textoRecebido = "";
+    private String textoBinario = "Primeiro envie uma resposta para obter uma resposta";
     private ProgressBar pb;
-    private int progressStatus = 0;
+    private int progressStatus, end;
+
     private Handler handler = new Handler();
 
 
@@ -42,7 +47,8 @@ public class CorreioEletronicoModemsFragment extends MyFragments {
         View view = inflater.inflate(R.layout.fragment_correio_eletronico_modems, container, false);
         initViews(view);
         initVerify();
-        nomeBinario.setText(textoRecebido);
+        nomeBinario.setText(textoBinario);
+
         mDicas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -53,16 +59,23 @@ public class CorreioEletronicoModemsFragment extends MyFragments {
         mEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textoNome = mTextoNome.getText().toString();
-                envinado.setVisibility(View.VISIBLE);
-                showProgressBar();
+                if (mTextoNome.getText().toString().isEmpty()){
+                    onCreateDialog("Algo deu errado", "Parece que você esqueceu de digitar o seu nome", R.drawable.ic_error_outline_black_24dp);
+                }else {
+                    textoNome = mTextoNome.getText().toString();
+                    nomeBinario.setVisibility(View.GONE);
+                    envinado.setVisibility(View.VISIBLE);
+                    showProgressBar();
+                }
+
             }
         });
 
         mFinalizar.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                textoRecebido = nomeBinario.getText().toString();
+                textoBinario = nomeBinario.getText().toString();
                 if (respostasIsEmpty()) {
                     onCreateDialog("Algo deu errado", getString(R.string.texto_alert_sem_resposta), R.drawable.ic_error_outline_black_24dp);
                 } else {
@@ -74,27 +87,6 @@ public class CorreioEletronicoModemsFragment extends MyFragments {
         return view;
     }
 
-    @Override
-    protected void initViews(View view) {
-        initEditTexts(view);
-        initTextView(view);
-        pb = (ProgressBar) view.findViewById(R.id.pb);
-        mDicas = (ImageButton) view.findViewById(R.id.dicasCEM);
-        mEnviar = (Button) view.findViewById(R.id.enviar);
-        mFinalizar = (Button) view.findViewById(R.id.btn_finalizar_cem);
-    }
-
-    private void initEditTexts(View view) {
-        textInputLayout1 = (TextInputLayout) view.findViewById(R.id.textInputLayout1);
-        textInputLayout2 = (TextInputLayout) view.findViewById(R.id.textInputLayout2);
-        mTextoTraduzido = (TextInputEditText) view.findViewById(R.id.textoTraduzidoCem);
-        mTextoNome = (TextInputEditText) view.findViewById(R.id.textoNome);
-    }
-
-    private void initTextView(View view) {
-        envinado = (TextView) view.findViewById(R.id.enviando);
-        nomeBinario = (TextView) view.findViewById(R.id.nomeBinario);
-    }
 
     @Override
     protected boolean respostasIsEmpty() {
@@ -112,7 +104,7 @@ public class CorreioEletronicoModemsFragment extends MyFragments {
     protected boolean validarCampos() {
         View focus = null;
         exibir = false;
-        if(!validarTexto(textoTraduzido)){
+        if (!validarTexto(textoTraduzido)) {
             mTextoTraduzido.setError(getString(R.string.resposta_incorreta));
             focus = mTextoTraduzido;
             exibir = true;
@@ -139,7 +131,7 @@ public class CorreioEletronicoModemsFragment extends MyFragments {
     }
 
     private boolean validarTexto(String s) {
-        if (s.equalsIgnoreCase("ola, " + textoNome)) {
+        if (removeAccents(s).equalsIgnoreCase("ola, " + textoNome)) {
             return true;
         }
         return false;
@@ -149,18 +141,21 @@ public class CorreioEletronicoModemsFragment extends MyFragments {
         TabelaAdapter tabela = new TabelaAdapter();
         String nomeBinarios = "01111 01100 00001, ";
         for (int i = 0; i < mTextoNome.length(); i++) {
-            nomeBinarios += tabela.obterBinarioDaLetra(mTextoNome.charAt(i)+"")+" ";
+            nomeBinarios += tabela.obterBinarioDaLetra(mTextoNome.charAt(i) + "") + " ";
         }
         return nomeBinarios;
     }
 
     private void showProgressBar() {
+        String textoConvertido = converterBinario(removeAccents(textoNome.replace(" ", "")));
         progressStatus = 0;
+        end = 0;
         new Thread(new Runnable() {
             @Override
             public void run() {
                 while (progressStatus < 100) {
                     // Update the progress status
+
                     progressStatus += 1;
 
                     // Try to sleep the thread for 20 milliseconds
@@ -169,7 +164,6 @@ public class CorreioEletronicoModemsFragment extends MyFragments {
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-
                     // Update the progress bar
                     handler.post(new Runnable() {
                         @Override
@@ -179,11 +173,44 @@ public class CorreioEletronicoModemsFragment extends MyFragments {
                             envinado.setText(progressStatus + "%");
                         }
                     });
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            nomeBinario.setVisibility(View.VISIBLE);
+                            nomeBinario.setText(textoConvertido);
+                        }
+                    }, 1900);// Start the operation
                 }
-                nomeBinario.setText(converterBinario(textoNome.toString()));
-
             }
-        }).start();// Start the operation
+        }).start();
 
+    }
+
+    private static String removeAccents(String str) {
+        str = Normalizer.normalize(str, Normalizer.Form.NFD);
+        str = str.replaceAll("[^\\p{ASCII}]", "");
+        return str;
+    }
+
+    @Override
+    protected void initViews(View view) {
+        initEditTexts(view);
+        initTextView(view);
+        pb = (ProgressBar) view.findViewById(R.id.pb);
+        mDicas = (ImageButton) view.findViewById(R.id.dicasCEM);
+        mEnviar = (Button) view.findViewById(R.id.enviar);
+        mFinalizar = (Button) view.findViewById(R.id.btn_finalizar_cem);
+    }
+
+    private void initEditTexts(View view) {
+        textInputLayout1 = (TextInputLayout) view.findViewById(R.id.textInputLayout1);
+        textInputLayout2 = (TextInputLayout) view.findViewById(R.id.textInputLayout2);
+        mTextoTraduzido = (TextInputEditText) view.findViewById(R.id.textoTraduzidoCem);
+        mTextoNome = (TextInputEditText) view.findViewById(R.id.textoNome);
+    }
+
+    private void initTextView(View view) {
+        envinado = (TextView) view.findViewById(R.id.enviando);
+        nomeBinario = (TextView) view.findViewById(R.id.nomeBinario);
     }
 }
